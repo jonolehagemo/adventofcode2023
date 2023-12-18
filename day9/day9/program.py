3644154efe8dc0f1e5f1cc5ec7e4bc96ea109bc4
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-12-18 08:57:27 +0100

    Update program.py

--- day9/day9/program.py
+++ day9/day9/program.py
@@ -6,18 +6,18 @@ def get_input(filepath: str) -> list[[int]]:
         return [list(map(int, line.split())) for line in filehandle.read().split("\n")]
 
 
-def extrapolate(values: list[int]) -> list[int]:
-    if not any(values):
-        return [0, *values, 0]
-    subs = extrapolate([right - left for left, right in pairwise(values)])
-    return [values[0] - subs[0], *values, values[-1] + subs[-1]]
+def extrapolate(_values: list[int]) -> list[int]:
+    if not any(_values):
+        return [0, *_values, 0]
+    extrapolated = extrapolate([right - left for left, right in pairwise(_values)])
+    return [_values[0] - extrapolated[0], *_values, _values[-1] + extrapolated[-1]]
 
 
-def process(data: list[list[int]], index: int) -> int:
-    return sum([extrapolate(ints)[index] for ints in data])
+def process(_data: list[list[int]], index: int) -> int:
+    return sum([extrapolate(ints)[index] for ints in _data])
 
 
 if __name__ == '__main__':
-    data1 = get_input('input.txt')
-    print(process(data1, 0))
-    print(process(data1, -1))
+    data = get_input('input.txt')
+    print(process(data, 0))
+    print(process(data, -1))
