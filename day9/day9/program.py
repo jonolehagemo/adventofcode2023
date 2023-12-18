84ec641f0c8a3fe4d1f20c576fcc901d4d098533
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-12-18 02:58:35 +0100

    Add day 9

---
+++ day9/day9/program.py
@@ -0,0 +1,23 @@
+from itertools import pairwise
+
+
+def get_input(filepath: str) -> list[[int]]:
+    with open(filepath) as filehandle:
+        return [list(map(lambda x: int(x), line.split(" "))) for line in filehandle.read().split("\n")]
+
+
+def extrapolate(values: list[int]) -> list[int]:
+    if not any(values):
+        return [0, *values, 0]
+    subs = extrapolate([right - left for left, right in pairwise(values)])
+    return [values[0] - subs[0], *values, values[-1] + subs[-1]]
+
+
+def process(data: list[list[int]], index: int) -> int:
+    return sum([extrapolate(ints)[index] for ints in data])
+
+
+if __name__ == '__main__':
+    data1 = get_input('input.txt')
+    print(process(data1, 0))
+    print(process(data1, -1))
---
+++ day9/tests/test_program.py
@@ -0,0 +1,19 @@
+import pytest
+
+from day9.day9.program import get_input, process, extrapolate
+
+
+def test_get_input():
+    assert get_input("input1.txt") == [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
+
+
+def test_extrapolate():
+    assert extrapolate([0, 3, 6, 9, 12, 15]) == [-3, 0, 3, 6, 9, 12, 15, 18]
+
+
+@pytest.mark.parametrize("index,expected", [
+    (-1, 114),
+    (0, -22),
+])
+def test_process(index, expected):
+    assert process(get_input("input1.txt"), index) == expected
